60458fdd743a313ae1cf43bb626a6de1eded8842
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-03-25 02:03:18 +0000

    Fixed the angle of the missiles

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -20,6 +20,7 @@ public class Player : FauxGravityBody {
 		transform.Rotate (0, Input.GetAxis ("Mouse X") * rotationSpeed, 0);
 		if (Input.GetMouseButtonDown(0)) {
 			GameObject missile = Instantiate (missilePrefab, transform.position, Quaternion.identity);
+			missile.transform.rotation = transform.rotation;
 			missile.transform.Rotate (90, 0, 0);
 			MissileMovement script = missile.GetComponent<MissileMovement> ();
 			script.center = attractor.transform;
