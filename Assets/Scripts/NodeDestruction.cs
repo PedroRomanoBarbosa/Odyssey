2696ab43df921d37b54aefc35db1ca79d8aa5d1c
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-03-30 19:44:07 +0100

    Added node

---
+++ Assets/Scripts/NodeDestruction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeDestruction : MonoBehaviour {
+	private int state;
+
+	void Start () {
+		state = 2;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (state == 0) {
+			Destroy (this.gameObject);
+		} else {
+			transform.GetChild (state).GetComponent<Renderer> ().enabled = false;
+			state -= 1;
+			transform.GetChild (state).GetComponent<Renderer> ().enabled = true;
+		}
+	}
+}
