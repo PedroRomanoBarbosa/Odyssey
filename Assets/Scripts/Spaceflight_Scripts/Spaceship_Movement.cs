882df44248cdb8e8b6056ce4800f07bf45d4eaed
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2017-04-27 20:07:53 +0100

    [Space] Planetoids + Initial Movement Test

---
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship_Movement : MonoBehaviour
+{
+    private float shipSpeed;
+    public float minSpeed = 0f;
+    public float maxSpeed = 100f;
+
+    void Start()
+    {
+        shipSpeed = minSpeed;
+    }
+    
+    void Update()
+    {
+        //Forward Movement Test
+        if (Input.GetKey(KeyCode.W))
+        {
+            ;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            ;
+        }
+        transform.Translate(0, 0, Time.deltaTime * shipSpeed);
+        
+    }
+}
