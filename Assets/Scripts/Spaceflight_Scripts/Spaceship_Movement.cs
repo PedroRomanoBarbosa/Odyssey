ee34458f93829a082339403f4500d40fd29bc196
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2017-04-27 20:52:57 +0100

    Aceleration and deceleration with the mouse

--- Assets/Scripts/Spaceflight_Scripts/Spaceship_Movement.cs
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_Movement.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Spaceship_Movement : MonoBehaviour
 {
-    private float shipSpeed;
+    public float shipSpeed;
     public float minSpeed = 0f;
-    public float maxSpeed = 100f;
+    public float maxSpeed = 50f;
+
+    private float acceleration, deceleration;
+
+    public Text display_speed;
 
     void Start()
     {
@@ -15,16 +20,25 @@ public class Spaceship_Movement : MonoBehaviour
     
     void Update()
     {
+        acceleration = maxSpeed - shipSpeed;
+        deceleration = shipSpeed - minSpeed;
+
         //Forward Movement Test
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            ;
+            shipSpeed += acceleration * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.Mouse1))
         {
-            ;
+            shipSpeed -= deceleration * Time.deltaTime;
         }
-        transform.Translate(0, 0, Time.deltaTime * shipSpeed);
         
+
+        transform.Translate(0, 0, Time.deltaTime * shipSpeed);
+
+        //Update Speed Display
+        if(display_speed != null)
+            display_speed.text = "Speed: " + shipSpeed.ToString();
+
     }
 }
