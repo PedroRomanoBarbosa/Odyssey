c37b6796cf7a03a824ff113e466188c35f58e7c9
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-04-29 21:13:51 +0100

    Added editor script to help place objects in the planet

---
+++ Assets/Scripts/EditorPlacementHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class EditorPlacementHelper : MonoBehaviour {
+	public Transform planet;
+	private GameObject selected;
+	public bool enable;
+
+	void Update () {
+		if(enable) {
+			selected = Selection.activeTransform.gameObject;
+			Vector3 direction = selected.transform.position - planet.transform.position;
+			Debug.DrawRay (planet.transform.position, direction, Color.red);
+			selected.transform.up = direction;
+		}
+	}
+}
