0d7460f0c056e29f90819f2dd2c54ec4dd6b2bd7
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-04-29 17:46:16 +0100

    Added a new scene for first planet and a skybox

---
+++ Assets/Scripts/PlanetMode/Action.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Action : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public virtual void OnAction () {
+		
+	}
+}
---
+++ Assets/Scripts/PlanetMode/GravityZone.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityZone : MonoBehaviour {
+	public float gravityRotationSpeed = 1f;
+}
