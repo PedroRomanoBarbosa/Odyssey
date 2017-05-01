736ea6bb33d70074be316c825b15da1ea9f74ab1
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2017-05-01 17:26:52 +0100

    Camera orbits ship, ship emulates camera rotation

---
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_Camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship_Camera : MonoBehaviour {
+
+	public GameObject playerShip;
+	Transform playerTransform;
+
+	private Vector3 mouseCenter;
+	float noTurn = 0.1f;
+	int factor = 150;
+
+	void Start(){
+		playerTransform = playerShip.transform; 
+
+		//Find the point in the center of the screen
+		mouseCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+	}
+	void Update () {
+
+		//Figure out how far from the center of the screen the mouse is
+		//and rotate camera smoothly to that spot 
+		var delta = (Input.mousePosition - mouseCenter) / Screen.height;
+		if (delta.y > noTurn) 
+			transform.Rotate(-(delta.y - noTurn) * Time.deltaTime * factor, 0, 0);
+		if (delta.y < -noTurn)
+			transform.Rotate(-(delta.y + noTurn) * Time.deltaTime * factor, 0, 0);
+		if (delta.x > noTurn)
+			transform.Rotate(0, (delta.x - noTurn) * Time.deltaTime * factor, 0);
+		if (delta.x < -noTurn)
+			transform.Rotate(0, (delta.x + noTurn) * Time.deltaTime * factor, 0); 
+
+		//Find a point behind and above the player ship and go there smoothly
+		Vector3 targetPosition = playerTransform.position - playerTransform.forward * 5 + playerTransform.up * 1;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime*50);
+ 	}
+  	
+
+}
--- Assets/Scripts/Spaceflight_Scripts/Spaceship_Movement.cs
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Spaceship_Movement : MonoBehaviour
 {
+    public GameObject shipCamera;
+
     public float shipSpeed;
     public float minSpeed = 0f;
     public float maxSpeed = 50f;
@@ -18,12 +20,13 @@ public class Spaceship_Movement : MonoBehaviour
         shipSpeed = minSpeed;
     }
     
-    void Update()
+    void LateUpdate()
     {
+        
+
+        //Forward Movement
         acceleration = maxSpeed - shipSpeed;
         deceleration = shipSpeed - minSpeed;
-
-        //Forward Movement Test
         if (Input.GetKey(KeyCode.Mouse0))
         {
             shipSpeed += acceleration * Time.deltaTime;
@@ -32,13 +35,14 @@ public class Spaceship_Movement : MonoBehaviour
         {
             shipSpeed -= deceleration * Time.deltaTime;
         }
-        
-
         transform.Translate(0, 0, Time.deltaTime * shipSpeed);
+        
+        //Update directional facing based on camera
+        transform.rotation = Quaternion.Lerp(transform.rotation, shipCamera.transform.rotation, Time.deltaTime * 1000);
 
         //Update Speed Display
         if(display_speed != null)
             display_speed.text = "Speed: " + shipSpeed.ToString();
-
+        
     }
 }
