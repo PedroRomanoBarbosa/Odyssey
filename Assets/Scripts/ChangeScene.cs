d6d2ee8fb3f63d2022719ecae4fa7a1d9ef5d851
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-05-02 11:19:21 +0100

    Added scene teleporters

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+	public string sceneName;
+
+	void OnTriggerEnter (Collider collider) {
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
