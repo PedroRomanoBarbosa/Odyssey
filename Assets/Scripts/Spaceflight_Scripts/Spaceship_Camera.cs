c3f1d847e2a89df0715710ab5a5594c889453f2a
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2017-05-05 15:48:28 +0100

    Scaled the vert/hor shiting, and added romano's ring

--- Assets/Scripts/Spaceflight_Scripts/Spaceship_Camera.cs
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_Camera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spaceship_Camera : MonoBehaviour {
-	
+
 	//Some access to the ship script will be required.
 	public GameObject playerShip;
 	Transform playerTransform;
@@ -12,7 +12,7 @@ public class Spaceship_Camera : MonoBehaviour {
 	private Vector3 mouseCenter;
 	float noTurn = 0.1f; //If the mouse is within the middle 10% of the screen, it shouldn't turn.
 	int planarFactor = 150; //Controls how fast the camera shifts its angle horizontally and vertically
-	float rotationFactor = 10; //Controls how fast you can rotate the view with Q/E
+	int rotationFactor = 10; //Controls how fast you can rotate the view with Q/E
 
 	//The smaller this value, the further behind the camera will be while chasing the player.
 	//Should be between 0 and 1
@@ -28,7 +28,8 @@ public class Spaceship_Camera : MonoBehaviour {
 	void Update () {
 
 		//How fast the ship can roll should depend on the ship's speed. But with a minimum value of rotation
-		rotationFactor = playerScript.shipForwardSpeed<10 ? 10f : playerScript.shipForwardSpeed; 
+		planarFactor = playerScript.shipForwardSpeed<10 ? 30 : (int)playerScript.shipForwardSpeed*3;
+		rotationFactor = playerScript.shipForwardSpeed<10 ? 10 : (int)playerScript.shipForwardSpeed; 
 
 		//Figure out how far from the center of the screen the mouse is
 		//and rotate camera smoothly to that spot 
