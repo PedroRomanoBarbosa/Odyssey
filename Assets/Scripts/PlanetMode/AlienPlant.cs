ee4b55cadbb4ef9e92913c451eb647e32a9e02f4
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-05-08 15:44:49 +0100

    Added watering can item and imported a low poly gems package

--- Assets/Scripts/PlanetMode/AlienPlant.cs
+++ Assets/Scripts/PlanetMode/AlienPlant.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AlienPlant : MonoBehaviour {
 	private float counter;
 	private float heightCounter;
+	private bool active;
 
 	public float endHeight;
 	public float speed;
@@ -12,13 +13,23 @@ public class AlienPlant : MonoBehaviour {
 	void Start () {
 		counter = 0;
 		heightCounter = 0;
+		active = false;
 	}
 
 	void Update () {
-		counter += Time.deltaTime;
-		heightCounter += speed * Time.deltaTime;
-		if (heightCounter < endHeight) {
-			transform.Translate (0, speed * Time.deltaTime, 0);
+		if (active) {
+			counter += Time.deltaTime;
+			heightCounter += speed * Time.deltaTime;
+			if (heightCounter < endHeight) {
+				transform.Translate (0, speed * Time.deltaTime, 0);
+			}
 		}
 	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.gameObject.CompareTag("Player")) {
+			active = true;
+		}
+	} 
+
 }
