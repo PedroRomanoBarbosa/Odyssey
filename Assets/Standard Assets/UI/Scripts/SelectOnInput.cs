23fa23bb3dbeadd471e522bbfcdce77012053cf3
Jorge <up201305617@fe.up.pt>
2017-05-15 15:25:38 +0100

    Main Menu

---
+++ Assets/Standard Assets/UI/Scripts/SelectOnInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    private bool buttonSeleted;
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetAxisRaw("Vertical")!=0 && buttonSeleted == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSeleted = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSeleted = false;
+    }
+}
