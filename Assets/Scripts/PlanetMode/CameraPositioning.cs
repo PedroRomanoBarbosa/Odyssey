18e70ce8e245ff6fee0e9b834bba2dad34805cec
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-05-16 23:33:06 +0100

    Added dynamic camera

---
+++ Assets/Scripts/PlanetMode/CameraPositioning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioning : MonoBehaviour {
+	private float defaultDistance;
+
+	public Transform player;
+
+	void Start () {
+		defaultDistance = (transform.position - player.position).magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = transform.position - player.position;
+		RaycastHit hit;
+		if (Physics.Raycast(player.position, direction, out hit)) {
+			transform.position = player.position + (direction.normalized * hit.distance);
+		}
+	}
+}
