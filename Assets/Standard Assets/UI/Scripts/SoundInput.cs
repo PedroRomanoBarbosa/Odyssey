0fa8cdb3744232b899069fa96369d9432df7fac3
Jorge <up201305617@fe.up.pt>
2017-05-16 10:48:56 +0100

    sound slider printing value

---
+++ Assets/Standard Assets/UI/Scripts/SoundInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundInput : MonoBehaviour
+{
+    public Slider soundSlider;
+    public Slider effectsSlider;
+    public Button button;
+
+    void Start ()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(SubmitSliderValue);
+    }
+
+	void Update ()
+    {
+		
+	}
+
+    void SubmitSliderValue()
+    {
+        Debug.Log(soundSlider.value);
+        Debug.Log(effectsSlider.value);
+    }
+}
