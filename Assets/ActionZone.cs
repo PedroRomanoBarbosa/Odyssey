c122a95857ce53d7b5f3d6ac5553485b77991e5f
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-05-21 17:29:51 +0100

    Added control panel to deactivate beams

---
+++ Assets/ActionZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionZone : MonoBehaviour {
+	private bool playerAround;
+
+	public Action action;
+
+	void Update () {
+		if (playerAround) {
+			if (Input.GetAxisRaw ("Use") == 1) {
+				action.OnAction ();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.CompareTag("Player")) {
+			playerAround = true;
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.CompareTag("Player")) {
+			playerAround = false;
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlanetMode/Deactivator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivator : Action {
+	private static int counter = 2;
+	private bool used;
+
+	public GameObject[] objects;
+	public Action action;
+
+	public override void OnAction () {
+		if (!used) {
+			used = true;
+			foreach (GameObject obj in objects) {
+				obj.SetActive (false);
+				counter--;
+				if (counter <= 0) {
+					action.OnAction ();
+				}
+			}
+		}
+	}
+}
