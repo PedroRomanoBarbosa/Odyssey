fccb387abc52f52b05b488d4c364f51b84e24edb
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2017-06-04 18:40:01 +0100

    Can shoot barriers

--- Assets/Scripts/Spaceflight_Scripts/Space_MissileLogic.cs
+++ Assets/Scripts/Spaceflight_Scripts/Space_MissileLogic.cs
@@ -7,18 +7,27 @@ public class Space_MissileLogic : MonoBehaviour {
 	public float duration;
 	private float timer = 0;
 
-
-
-
 	void Update () {
-
 		//Basic Forward Momentum
 		transform.position += transform.forward * speed * Time.deltaTime;
-
 		//Check if it's dead
 		timer += Time.deltaTime;
 		if(timer > duration)
 			Destroy(this.gameObject);
 		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("PlanetBarrier")){
+			Debug.Log("Shot a barrier!");
+			//Destroy the barrier and the missile
+			PlanetSelectionVars vars = other.transform.parent.transform.GetComponent<PlanetSelectionVars>();
+			vars.barrier = false;
+			Destroy(this.gameObject);
+
+			//Very shitty way to do this...
+			other.gameObject.SetActive(false);
+		}
 	}
 }
--- Assets/Scripts/Spaceflight_Scripts/Spaceship_MissileLaucher.cs
+++ Assets/Scripts/Spaceflight_Scripts/Spaceship_MissileLaucher.cs
@@ -39,9 +39,5 @@ public class Spaceship_MissileLaucher : MonoBehaviour {
 		}
 			
 	}
-	void OnTriggerEnter(Collider other)
-	{
-		
-	}
 
 }
