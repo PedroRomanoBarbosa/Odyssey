325c20f85dd571d57aad390803f303442e83f392
Jorge <up201305617@fe.up.pt>
2017-06-09 11:49:32 +0100

    changes

---
+++ Assets/Scripts/UI Script/GemDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemDisplay : MonoBehaviour
+{
+    public GameObject cuboid;
+    public GameObject star;
+    public GameObject penta;
+    public GameObject spiral;
+
+	void Update ()
+    {
+		
+	}
+}
