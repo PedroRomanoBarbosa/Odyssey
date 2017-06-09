a8f5fadb106bcb8bacf520226b99a3cf3b92824b
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-06-09 17:27:48 +0100

    Added shiplanding prefab. Has bugs

---
+++ Assets/Scripts/PlanetMode/ShipLanding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipLanding : MonoBehaviour {
+	public Camera camera;
+	public GameObject ship;
+
+	void Start () {
+		if (GameVariables.shipFirstPlanet) {
+			camera.enabled = true;
+			GameVariables.cinematicPaused = true;
+		}
+	}
+
+	void ShipLanded () {
+		GameVariables.cinematicPaused = false;
+		camera.enabled = false;
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ().ShowModel ();
+	}
+
+}
