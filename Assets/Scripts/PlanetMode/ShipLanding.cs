021e4e771bba55edff1b8a085a17992bc04cdb65
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-06-09 18:59:53 +0100

    Improved landing

--- Assets/Scripts/PlanetMode/ShipLanding.cs
+++ Assets/Scripts/PlanetMode/ShipLanding.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShipLanding : MonoBehaviour {
 	private float aniCounter;
 	private Vector3 startPosition;
 	private Quaternion startRotation;
+	private bool playerInArea;
+	private bool reverse;
 
 	public Camera camera;
 	public GameObject ship;
@@ -22,16 +25,39 @@ public class ShipLanding : MonoBehaviour {
 	}
 
 	void Update () {
+		// If player has already the ship
 		if (GameVariables.shipFirstPlanet) {
 			if (aniCounter > duration) {
-				transform.position = Vector3.Slerp (startPosition, end.position, 1f);
-				transform.rotation = Quaternion.Slerp (startRotation, end.rotation, 1f);
-				ShipLanded ();
+				if (reverse) {
+					transform.position = startPosition;
+					transform.rotation = startRotation;
+					SceneManager.LoadScene ("spaceship_control_test_ui");
+				} else {
+					transform.position = end.position;
+					transform.rotation = end.rotation;
+					ShipLanded ();
+				}
 			} else {
 				aniCounter += Time.deltaTime;
-				float t = aniCounter / duration;
+				float t;
+				if (reverse) {
+					t = 1 - aniCounter / duration;
+				} else {
+					t = aniCounter / duration;
+				}
 				transform.position = Vector3.Slerp (startPosition, end.position, t);
 				transform.rotation = Quaternion.Slerp (startRotation, end.rotation, t);
+			}
+
+			// Action to leave planet
+			if (playerInArea) {
+				if (Input.GetAxisRaw ("Use") == 1) {
+					reverse = true;
+					camera.enabled = true;
+					GameVariables.cinematicPaused = true;
+					aniCounter = 0;
+					GameObject.FindGameObjectWithTag("Player").GetComponent<Player> ().HideModel ();
+				}
 			}
 		}
 	}
@@ -40,6 +66,18 @@ public class ShipLanding : MonoBehaviour {
 		GameVariables.cinematicPaused = false;
 		camera.enabled = false;
 		GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ().ShowModel ();
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.CompareTag ("Player")) {
+			playerInArea = true;
+		}
+	}
+
+	void OnTriggerExit (Collider collider) {
+		if (collider.CompareTag ("Player")) {
+			playerInArea = false;
+		}
 	}
 
 }
