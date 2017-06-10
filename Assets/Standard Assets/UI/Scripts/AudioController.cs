12f9b7a1b1e7e5f1842bfe3b994e979e35f3a8ba
Jorge <up201305617@fe.up.pt>
2017-06-10 10:08:11 +0100

    volume sons ajustável - inicio

---
+++ Assets/Standard Assets/UI/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private int musicVolume;
+    private int effectsVolume;
+
+	void Start ()
+    {
+        musicVolume = 1;
+        effectsVolume = 1;
+	}
+	
+    void Update()
+    {
+        Debug.Log(musicVolume);
+    }
+
+	public void SetMusicVolume(int v)
+    {
+        musicVolume = v;
+    }
+
+    public void SetEffectsVolume(int v)
+    {
+        effectsVolume = v;
+    }
+
+    public int GetMusicVolume()
+    {
+        return musicVolume;
+    }
+
+    public int GetEffectsVolume()
+    {
+        return effectsVolume;
+    }
+}
