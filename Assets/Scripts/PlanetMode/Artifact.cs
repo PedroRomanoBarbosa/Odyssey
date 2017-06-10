918d18ec7be532ca29f40c150e1998b89569d402
Jorge <up201305617@fe.up.pt>
2017-06-10 10:30:17 +0100

    catch spiral

--- Assets/Scripts/PlanetMode/Artifact.cs
+++ Assets/Scripts/PlanetMode/Artifact.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Artifact : MonoBehaviour {
+public class Artifact : MonoBehaviour
+{
 	public GameVariables.Artifact artifact;
 
-	void OnTriggerEnter (Collider collider) {
-		if (collider.gameObject.CompareTag("Player")) {
+	void OnTriggerEnter (Collider collider)
+    {
+		if (collider.gameObject.CompareTag("Player"))
+        {
 			GameVariables.artifacts [(int)artifact] = true;
             CatchArtifact(artifact);
             Destroy (gameObject);
@@ -15,18 +18,23 @@ public class Artifact : MonoBehaviour {
 
     void CatchArtifact(GameVariables.Artifact a)
     {
-        if(a.ToString() == "Cuboid")
+        FindObjectOfType<Player>().pts += 250;
+
+        if (a.ToString() == "Cuboid")
         {
             FindObjectOfType<GemDisplay>().SetCatchCuboid(true);
         }
+
         if (a.ToString() == "Star")
         {
             FindObjectOfType<GemDisplay>().SetCatchStar(true);
         }
+
         if (a.ToString() == "Penthagon")
         {
             FindObjectOfType<GemDisplay>().SetCatchPenta(true);
         }
+
         if (a.ToString() == "Spiral")
         {
             FindObjectOfType<GemDisplay>().SetCatchSpiral(true);
