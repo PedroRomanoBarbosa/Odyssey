a8499e490db6653c2ced80c8ee4ba4718a0a23c3
Jorge <up201305617@fe.up.pt>
2017-06-13 16:11:06 +0100

    comment

--- Assets/Scripts/PlanetMode/Player.cs
+++ Assets/Scripts/PlanetMode/Player.cs
@@ -89,7 +89,7 @@ public class Player : FauxGravityBody {
 		equippedTools = new List<Tool> ();
 		for (int i = 0; i < GameVariables.tools.Length; i++) {
 			if (GameVariables.tools [i]) {
-				//equippedTools.Add (tools[i]);
+				equippedTools.Add (tools[i]);
 			}
 		}
 		toolIndex = 0;
