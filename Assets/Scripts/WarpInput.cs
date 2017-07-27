a0c8bfd25059c1e4876eedba4a20f469e1e42ac5
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-07-27 17:13:30 +0100

    Added warp input

---
+++ Assets/Scripts/WarpInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarpInput : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown ("1")) {
+			SceneManager.LoadSceneAsync ("FirstPlanet");
+		} else if (Input.GetKeyDown ("2")) {
+			SceneManager.LoadSceneAsync ("SmallPlanet");
+		} else if (Input.GetKeyDown ("3")) {
+			SceneManager.LoadSceneAsync ("VulcanicPlanet");
+		} else if (Input.GetKeyDown ("4")) {
+			SceneManager.LoadSceneAsync ("JunglePlanet");
+		} else if (Input.GetKeyDown ("5")) {
+			SceneManager.LoadSceneAsync ("FinalPlanet");
+		}
+	}
+}
